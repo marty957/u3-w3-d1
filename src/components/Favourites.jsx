90a91f2873ea6c7b0d85a4e6dd9d93ce66749bba
marty957 <fantuzzimartina@yahoo.it>
2024-12-09 16:40:17 +0100

    U3-W3-D1

---
+++ src/components/Favourites.jsx
@@ -0,0 +1,29 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+const Favourites = () => {
+  const companies = useSelector((state) => state.companies.content);
+  return (
+    <>
+      <Container className="text-center">
+        <h1 className="display-1">Favourites</h1>
+      </Container>
+      <Row>
+        {companies.length > 0 ? (
+          companies.map((company, i) => (
+            <Col sm={12} key={i}>
+              {company}
+            </Col>
+          ))
+        ) : (
+          <Col sm={12} className="lead ">
+            <span className="text-primary opacity-50 fs-1 me-2"></span>
+            Your cart is empty
+          </Col>
+        )}
+      </Row>
+    </>
+  );
+};
+
+export default Favourites;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,19 @@
+const initialState = {
+  companies: {
+    content: []
+  }
+};
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAV":
+      return {
+        ...state,
+        companies: {
+          ...state.companies,
+          content: [...state.companies.content, action.payload]
+        }
+      };
+  }
+};
+export default mainReducer;
